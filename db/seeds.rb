aae05dc93657779c8301ceb6f6fbe98378280d09
Jason Quaccia <jlquaccia@gmail.com>
2015-09-06 19:16:32 -0700

    created seed data that builds dummy users and wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
+require 'faker'
+
+# Create users
+25.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.free_email,
+    password: Faker::Internet.password
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create wikis
+50.times do
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::Name.title,
+    body: Faker::Lorem.paragraph
+  )
+end
+
 # Create an admin user
 admin = User.new(
   name: 'Admin User',
@@ -30,3 +53,4 @@ standard.save!
 
 puts "Seed finished."
 puts "#{User.count} users created (Including 1 admin, 1 premium and 1 standard user)."
+puts "#{Wiki.count} wikis created."
